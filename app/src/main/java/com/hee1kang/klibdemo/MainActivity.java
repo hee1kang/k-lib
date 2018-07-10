016c78436d93b5ac0763a6640480c503d609271e
Eric <eric.kang@planetory.me>
2018-07-10 14:18:14 +0900

    add

--- app/src/main/java/com/hee1kang/klibdemo/MainActivity.java
+++ app/src/main/java/com/hee1kang/klibdemo/MainActivity.java
@@ -3,11 +3,14 @@ package com.hee1kang.klibdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hee1kang.klib.Tog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Tog.te("");
     }
 }
